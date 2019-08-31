cf45b3b058ec97722fe759d50fa7a026eb1b397d
Pietro Carrara <piticarrara@gmail.com>
2019-08-31 13:18:53 -0300

    Position the cards in a parbola

--- src/PlayerHand/PlayerHand.cs
+++ src/PlayerHand/PlayerHand.cs
@@ -30,9 +30,11 @@ namespace CardNovel.UI
             foreach (Card child in children)
             {
                 var maxIndex = total - 1;
+
                 // The leftmost card should have this value as -1,
-                // the middlemost should have this as 0,
-                // and the rightmost should be 1
+                // and the rightmost should be 1.
+                // The other cards will be indexed in order ranging
+                // from (-1, 1)
                 var range = (i - maxIndex / 2) / (maxIndex < 1 ? 1 : maxIndex / 2);
 
                 var cardSize = child.GetSize() * 0.5f;
@@ -40,12 +42,14 @@ namespace CardNovel.UI
                 // Center position
                 var position = (new Vector2(cardSize.x, 0) * i) -
                                ((new Vector2(total * cardSize.x, 0)) / 2);
+                // Position the cards in a parbola
+                position.y += Mathf.Pow(range * total, 2);
 
                 var rect = new Rect2(position, child.GetSize());
 
                 this.FitChildInRect(child, rect);
                 child.RectPivotOffset = new Vector2(rect.Size.x / 2f, rect.Size.y);
-                child.SetRotationDegrees(45 * range);
+                child.SetRotationDegrees(30 * range);
 
                 i++;
             }
